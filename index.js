5bdd2ab758a0c17ec85b9b608fb2becb6a5f0720
Chris <dribbleribbletribble@gmail.com>
2019-01-21 13:46:31 -0500

    Update
    Some code cleanup.

--- index.js
+++ index.js
@@ -1,153 +1,146 @@
 const qs = require('querystring');
-const rmvDeliminatorRGX = new RegExp(/\/+$/);
-const self = module.exports;
 const path = require('path');
 const fs = require('fs');
-const mime = require('mime-types')
+const mime = require('mime-types');
 
-let routes = [];
-let controllerPath = "";
+const rmvDeliminatorRGX = new RegExp(/\/+$/);
+const removeQSRegex = new RegExp(/.+?(?=\?)/g);
+const qsRegex = new RegExp(/(?<=\?).*/g);
 
-let HTML_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATH', 'ALL'];
+const self = module.exports;
 
-HTML_METHODS.forEach((m) => {
-    exports[m.toLowerCase()] = (route, action) => {
-        self.route(route, m, action);
-    }
+const routes = [];
+
+const HTTP_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATH', 'ALL'];
+
+HTTP_METHODS.forEach((m) => {
+  exports[m.toLowerCase()] = (route, action) => {
+    self.route(route, m, action);
+  };
 });
 
 const parseBody = (req, callback) => {
-    var body = '';
-    req.on('data', (data) => body += data);
-    req.on('end', () => callback(qs.parse(body)));
-}
+  let body = '';
+  req.on('data', (data) => {
+    body += data;
+  });
+  req.on('end', () => callback(qs.parse(body)));
+};
 
 exports.route = (route, method, action) => {
-    let routeParamRegex = new RegExp(/(\:).*?(?=\/|\/|\-|\.|$)/g);
-    route = route.replace(rmvDeliminatorRGX, "");
-    route = route.charAt(0) === "/" ? route : "/" + route;
+  const routeParamRegex = new RegExp(/(:).*?(?=\/|\/|-|\.|$)/g);
+  let r = route;
+  r = r.replace(rmvDeliminatorRGX, '');
+  r = r.charAt(0) === '/' ? r : `/${r}`;
 
-    let routeParams = route.match(routeParamRegex);
-    let regexRoute = route;
+  const params = r.match(routeParamRegex);
+  let regexRoute = r;
 
-    regexRoute = regexRoute.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+  regexRoute = regexRoute.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 
-    if (typeof action === "string") {
-        if (action.includes(".")) {
-            let [controllerName, actionName] = action.split(".");
-            try {
-                let controller = require(controllerPath + controllerName + ".js");
-                if (typeof controller[actionName] === 'undefined') throw new Error('Controller: \'' + controllerName + '\', Action: \'' + actionName + '\' not found.');
-                action = controller[actionName];
-            }
-            catch (e) {
-                throw e;
-            }
-        } else {
-            throw new Error('Invalid input in route action field, unsupported action method or invalid action method expression');
-        }
-    }
-
-    if (routeParams !== null) {
-        routeParams.forEach(element => {
-            regexRoute = regexRoute.replace(element, "(.*)");
-        });
-
-        routes.push({ regexRoute: regexRoute, route: route, method: method.toUpperCase(), action: action, params: routeParams });
-    } else {
-        routes.push({ route: route, method: method.toUpperCase(), action: action });
-    }
-
-    routes.sort((a, b) => {
-        if (a.regexRoute !== undefined) {
-            return 1;
-        } else {
-            return -1;
-        }
+  if (params !== null) {
+    params.forEach((element) => {
+      regexRoute = regexRoute.replace(element, '(.*)');
     });
-}
 
-const extractParams = (req, res, route) => {
-    req.params = {};
-    if (req.url.includes("?")) {
-        let qsRegex = new RegExp(/(?<=\?).*/g);
-        let qstring = req.url.match(qsRegex);
-        req.params = qs.parse(qstring[0], "&", "=");
-        return disperse(route, req, res);
+    routes.push({
+      regexRoute,
+      route,
+      method: method.toUpperCase(),
+      action,
+      params,
+    });
+  } else {
+    routes.push({
+      route,
+      method: method.toUpperCase(),
+      action,
+    });
+  }
+
+  routes.sort((a) => {
+    if (a.regexRoute !== undefined) {
+      return 1;
     }
-    if (req.method == "POST") {
-        parseBody(req, function (data) {
-            req.params = data;
-            return disperse(route, req, res);
-        });
-    }
-    if (req.method == "GET" && route.regexRoute !== undefined) {
-        let routeParamMatches = req.url.match(route.regexRoute);
-        for (let i = 0; i < route.params.length; i++) {
-            req.params[route.params[i].replace(":", "")] = routeParamMatches[i + 1];
-        }
-        return disperse(route, req, res);
-    }
-    return disperse(route, req, res);
-}
+    return -1;
+  });
+};
 
 const disperse = (route, req, res) => {
-    if (typeof route.action === "function") {
-        for (let mw in self.middleware) {
-            mw(req, res);
-        }
-        return route.action(req, res);
-    } else if (typeof route.action === 'undefined') {
-        //Hooks
+  if (typeof route.action === 'function') {
+    self.middleware.forEach((mw) => {
+      mw(req, res);
+    });
+    return route.action(req, res);
+  }
+  return false;
+};
+
+const extractParams = (req, res, route) => {
+  req.params = {};
+  if (req.url.includes('?')) {
+    const qstring = req.url.match(qsRegex);
+    req.params = qs.parse(qstring[0], '&', '=');
+    return disperse(route, req, res);
+  }
+  if (req.method === 'POST') {
+    parseBody(req, (data) => {
+      req.params = data;
+      return disperse(route, req, res);
+    });
+  }
+  if (req.method === 'GET' && route.regexRoute !== undefined) {
+    const routeParamMatches = req.url.match(route.regexRoute);
+    for (let i = 0; i < route.params.length; i += 1) {
+      req.params[route.params[i].replace(':', '')] = routeParamMatches[i + 1];
     }
-}
+    return disperse(route, req, res);
+  }
+  return disperse(route, req, res);
+};
 
 exports.middleware = [];
 
 exports.addMiddleWare = (cb) => {
-    self.middleware.push(cb);
-}
-
-exports.setControllerPath = (path) => {
-    controllerPath = path;
-}
+  self.middleware.push(cb);
+};
 
 exports.staticDir = (staticFolder) => {
-    self.staticFolder = staticFolder;
-}
+  self.staticFolder = staticFolder;
+};
 
 const serveStatic = (req, res, staticFile) => {
-    let mt = mime.lookup(staticFile);
-    if (fs.existsSync(staticFile) && !fs.lstatSync(staticFile).isDirectory() && mt !== false) {
-        res.writeHead(200, { 'Content-type': mt });
-        let data = fs.readFileSync(staticFile);
-        res.write(data);
-        res.end();
-    }
-}
+  const mt = mime.lookup(staticFile);
+  if (fs.existsSync(staticFile) && !fs.lstatSync(staticFile).isDirectory() && mt !== false) {
+    res.writeHead(200, { 'Content-type': mt });
+    const data = fs.readFileSync(staticFile);
+    res.write(data);
+    res.end();
+  }
+};
 
 exports.handle = (req, res) => {
-    let removeQSRegex = new RegExp(/.+?(?=\?)/g);
-    req.url = req.url == "/" ? req.url : req.url.replace(rmvDeliminatorRGX, "");
-    let staticFile = path.join(self.staticFolder, '..', req.url)
-
-    serveStatic(req, res, staticFile);
-
-    for (r in routes) {
-        let route = routes[r];
-        if ((req.url.match(removeQSRegex) == null ? route.route == req.url : req.url.match(removeQSRegex)[0] == route.route) && (req.method == route.method || route.method == "ALL")) {
-
-            return extractParams(req, res, route);
-        }
-        if (route.regexRoute !== undefined && req.url.match(route.regexRoute) !== null && (req.method == route.method || route.method == "ALL")) {
-
-            return extractParams(req, res, route);
-        }
+  req.url = req.url.replace(rmvDeliminatorRGX, '') === '' ? '/' : req.url.replace(rmvDeliminatorRGX, '');
+  const staticFile = path.join(self.staticFolder, '..', req.url);
+  serveStatic(req, res, staticFile);
+  let hit = false;
+  routes.forEach((route) => {
+    if ((req.url.match(removeQSRegex) == null ? route.route === req.url : req.url.match(removeQSRegex)[0] === route.route) && (req.method === route.method || route.method === 'ALL')) {
+      hit = true;
+      return extractParams(req, res, route);
     }
-
+    if (route.regexRoute !== undefined && req.url.match(route.regexRoute) !== null && (req.method === route.method || route.method === 'ALL')) {
+      hit = true;
+      return extractParams(req, res, route);
+    }
+    return route;
+  });
+  if (!hit) {
     res.writeHead(404, {
-        'Content-Length': Buffer.byteLength("404 NOT FOUND"),
-        'Content-Type': 'text/plain'
+      'Content-Length': Buffer.byteLength('404 NOT FOUND'),
+      'Content-Type': 'text/plain',
     });
-    res.end("404 NOT FOUND");
-}
+    return res.end('404 NOT FOUND');
+  }
+  return true;
+};
